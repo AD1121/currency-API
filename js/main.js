89725d375a4849effd3d5cdae36eb89d13278337
AD1121 <sinagh30@gmail.com>
2023-01-13 20:47:53 +0100

    comments added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+// Added html attributes
 const searchBtn = document.getElementById('search-btn')
 const tblBody = document.querySelector('table tbody')
 const searchInput1 = document.getElementById('search-input-one')
 const searchInput2 = document.getElementById('search-input-two')
+// Getting h3 to add the result of the both input
 const comparedCurrencies = document.getElementById('compared-currencies')
 
+// Function for the search button
 searchBtn.addEventListener('click', compared_currencies)
 
 async function list_all() {
@@ -13,12 +16,15 @@ async function list_all() {
     )
     const list = await response.json()
 
+    // Getting the value for the currency and country
     let currencyHTML = ''
     let countryHTML = ''
     for (let currency of Object.keys(list)) {
+      // Making sure that currency have a value, otherwise adding a '-' to it
       if (list[currency] === '') {
         list[currency] = '-'
       }
+
       countryHTML += `<li>${list[currency]}</li><hr/>`
       currencyHTML += `<li>${currency}</li><hr/>`
     }
@@ -38,34 +44,31 @@ async function list_all() {
             </tr>
         `
   } catch (error) {
-    console.log(error)
+    throw new Error(error)
   }
 }
 
 list_all()
 
+// Function that search button have in clikc event
 async function compared_currencies() {
   try {
     const response = await fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${searchInput1.value}/${searchInput2.value}.json`
     )
     const list = await response.json()
-    console.log(list)
 
     let countryHTML = ''
     for (let currency of Object.keys(list)) {
-      // if (list[currency] === '') {
-      //   list[currency] = '-'
-      // }
       countryHTML = list[currency]
-      console.log(countryHTML)
     }
 
+    // Adding the result into the h3 field
     comparedCurrencies.innerText = `1 ${searchInput1.value} = ${countryHTML} ${searchInput2.value}`
 
-    searchInput1.value = ''
-    searchInput2.value = ''
+    searchInput1.value = '' // empty the value of the first input
+    searchInput2.value = '' // empty the value of the second input
   } catch (error) {
-    console.log(error)
+    throw new Error(error)
   }
 }
