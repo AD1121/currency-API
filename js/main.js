b2f42515cb90d03c3a41c76af6f7af18088b12bc
AD1121 <sinagh30@gmail.com>
2023-01-13 20:39:48 +0100

    functions added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const searchBtn = document.getElementById('search-btn')
 const tblBody = document.querySelector('table tbody')
 const searchInput1 = document.getElementById('search-input-one')
 const searchInput2 = document.getElementById('search-input-two')
+const comparedCurrencies = document.getElementById('compared-currencies')
 
 searchBtn.addEventListener('click', compared_currencies)
 
@@ -42,7 +43,7 @@ async function list_all() {
 }
 
 list_all()
-// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json
+
 async function compared_currencies() {
   try {
     const response = await fetch(
@@ -51,31 +52,19 @@ async function compared_currencies() {
     const list = await response.json()
     console.log(list)
 
-    // let currencyHTML = ''
-    // let countryHTML = ''
-    // for (let currency of Object.keys(list)) {
-    //   if (list[currency] === '') {
-    //     list[currency] = '-'
-    //   }
-    //   countryHTML += `<li>${list[currency]}</li><hr/>`
-    //   currencyHTML += `<li>${currency}</li><hr/>`
-    //   console.log(currencyHTML, countryHTML)
-    // }
+    let countryHTML = ''
+    for (let currency of Object.keys(list)) {
+      // if (list[currency] === '') {
+      //   list[currency] = '-'
+      // }
+      countryHTML = list[currency]
+      console.log(countryHTML)
+    }
 
-    // tblBody.innerHTML += `
-    //         <tr>
-    //             <td>
-    //                 <ul>
-    //                     ${currencyHTML}
-    //                 </ul>
-    //             </td>
-    //             <td>
-    //                 <ul>
-    //                     ${countryHTML}
-    //                 </ul>
-    //             </td>
-    //         </tr>
-    //     `
+    comparedCurrencies.innerText = `1 ${searchInput1.value} = ${countryHTML} ${searchInput2.value}`
+
+    searchInput1.value = ''
+    searchInput2.value = ''
   } catch (error) {
     console.log(error)
   }
